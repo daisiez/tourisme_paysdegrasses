0dcc334652d0b142d70346516ded23627ec16ecd
Daisy Leroy <daisyradix@gmail.com>
2020-09-18 21:36:09 +0200

    init D8.9

--- scripts/drupal-8.6-to8.9.sh
+++ scripts/drupal-8.6-to8.9.sh
@@ -6,5 +6,5 @@ vendor/drush/drush/drush updb
 vendor/drush/drush/drush cr
 echo "update step 2"
 rm -rf modules/context modules/geolocation modules/modal_page modules/paragraphs
-vendor/drush/drush/drush updb
+vendor/drush/drush/drush updb 
 vendor/drush/drush/drush cr
