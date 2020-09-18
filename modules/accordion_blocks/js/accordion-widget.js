47915fbce9c6ff510f702757d946380ffb41e048
Daisy Leroy <daisyradix@gmail.com>
2020-09-18 21:30:07 +0200

    init D8.9

--- modules/accordion_blocks/js/accordion-widget.js
+++
@@ -1,5 +0,0 @@
-(function($) {  
-    $(document).ready(function(){
-       $('.accordion_blocks_container').accordion({header: "h2.accordion-title", autoHeight: false}); 
-    });
-})(jQuery);
---
+++ scripts/drupal-8.6-to8.9.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+composer install
+echo "update step 1"
+vendor/drush/drush/drush updb
+vendor/drush/drush/drush cr
+echo "update step 2"
+rm -rf modules/context modules/geolocation modules/modal_page modules/paragraphs
+vendor/drush/drush/drush updb
+vendor/drush/drush/drush cr
